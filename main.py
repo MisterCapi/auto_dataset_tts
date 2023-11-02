4fe59276ed0ba9447dc1b8d6ac00c3b6a7b25a17
MisterCapi <mistercapi0@gmail.com>
2023-11-02 18:47:55 +0100

    Add re-whispering to avoid leaving non-speech in samples

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import glob
 import os
+import shutil
 from datetime import datetime
 from typing import List
 
 import librosa
+import numpy as np
 import randomname
 import soundfile as sf
 import torch.cuda
@@ -11,13 +13,56 @@ import whisperx
 from tqdm import tqdm
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
-batch_size = 8 # reduce if low on GPU mem
-compute_type = "float16" # change to "int8" if low on GPU mem (may reduce accuracy)
+batch_size = 4              # reduce if low on GPU mem
+compute_type = "float16"    # change to "int8" if low on GPU mem (may reduce accuracy)
+language = "pl"
 
 # 1. Transcribe with original whisper (batched)
-model = whisperx.load_model("large-v2", device, compute_type=compute_type, language="pl")
+model = whisperx.load_model("large-v2", device, compute_type=compute_type, language=language)
+model_a, metadata = whisperx.load_align_model(language_code=language, device=device)
 
 run_name = datetime.now().strftime("%Y_%m_%d_%H_%M") + f"_{randomname.get_name()}"
+
+
+def cut_sample_to_speech_only(audio_path):
+    audio_sample = whisperx.load_audio(audio_path)
+    result_sample = model.transcribe(audio_sample, batch_size=batch_size, chunk_size=30)
+    result_sample = whisperx.align(result_sample["segments"], model_a, metadata, audio_sample, device)
+
+    try:
+        end = result_sample['segments'][-1]['end']
+    except IndexError:
+        print(f"Skipping {audio_path}")
+        return False
+
+    # Load audio with Librosa
+    audio, sr = librosa.load(audio_path)
+
+    # Calculate dB amplitude
+    amplitude_db = librosa.amplitude_to_db(np.abs(audio), ref=np.max)
+
+    # This logic is here to prevent whisper from cutting audio too short (this happened quite often in my experience)
+    # If you trust whisper 100% (or have noisy audio) -> commend all "optional" code below
+    # ======= OPTIONAL CODE =======
+    window_size_sec = 0.05
+    silence_threshold_db = -30
+    window_size_samples = int(window_size_sec * sr)
+    start_frame = int(end * sr)
+
+    audio_windows = [amplitude_db[x:x + window_size_samples] for x in
+                     range(start_frame, len(amplitude_db), window_size_samples)]
+
+    audio_windows = [np.max(x) for x in audio_windows]
+
+    for window in audio_windows:
+        if window > silence_threshold_db:
+            end += window_size_sec
+        else:
+            break
+    # ======= END OPTIONAL CODE =======
+
+    sf.write(audio_path, audio[:int(end * sr)], int(sr))
+    return True
 
 
 def cut_and_save_audio(input_audio_path, segments, target_sampling_rate=22050):
@@ -44,47 +89,47 @@ def cut_and_save_audio(input_audio_path, segments, target_sampling_rate=22050):
     # Cut and save audio segments
     outputs = []
     output_prefix = os.path.splitext(os.path.basename(input_audio_path))[0]
-    for idx, (start_sample, end_sample) in enumerate(segment_samples):
+    for idx, (start_sample, end_sample) in tqdm(enumerate(segment_samples), desc=input_audio_path,
+                                                total=len(segments), leave=False):
         audio_segment = audio[start_sample:end_sample]
         output_path = os.path.join(output_dir_name, f"{output_prefix}_{idx + 1}.wav")
         sf.write(output_path, audio_segment, target_sampling_rate)
-        outputs.append(output_path)
+        # We use whisper again on each sample to get rid of non-speech sounds
+        good_audio = cut_sample_to_speech_only(output_path)
+        if good_audio:
+            outputs.append(output_path)
+        else:
+            try:
+                os.remove(output_path)
+            except FileNotFoundError:
+                print(f"File '{output_path}' not found.")
+            except PermissionError:
+                print(f"You do not have permission to delete '{output_path}'.")
+            except Exception as e:
+                print(f"An error occurred: {e}")
 
     return outputs
 
 
 def create_segments_for_files(files_to_segment: List[str]):
 
-    for audio_file in tqdm(files_to_segment, desc="Transcribing and segmenting files"):
+    for audio_file in tqdm(sorted(files_to_segment, key=lambda x: os.path.getsize(x), reverse=True),
+                           desc="Transcribing and segmenting files"):
         audio = whisperx.load_audio(audio_file)
         result = model.transcribe(audio, batch_size=batch_size, chunk_size=30)
 
-        # 2. Align whisper output
-        model_a, metadata = whisperx.load_align_model(language_code=result["language"], device=device)
+        # Align the whisper output
         result = whisperx.align(result["segments"], model_a, metadata, audio, device, return_char_alignments=True)
 
-        # Save all segments as
-        for segment in result["segments"]:
-            char_end = False
-            for char_data in reversed(segment['chars']):
-                char_end = char_data.get('end', False)
-                if char_end and char_end < segment['end']:
-                    char_end = char_data.get('end')
-                    break
-            segment['char_end'] = char_end if char_end else segment['end']
-
-        segments = [[segment['start'], segment['char_end']] for segment in result["segments"]]
+        segments = [[segment['start'], segment['end']] for segment in result["segments"]]
         target_sampling_rate = 22050
 
         segment_filenames = cut_and_save_audio(audio_file, segments, target_sampling_rate)
 
-        for segment, segment_filename in zip(result["segments"], segment_filenames):
-            segment_filename = segment_filename.split('\\')[-2:]
-            segment_filename = os.path.join(*segment_filename)
-            segment['filename'] = segment_filename.replace('\\', '/')
-            segment['duration'] = segment['end'] - segment['start']
+        segment_filenames = [os.path.join(path.split(os.sep)[-2:]) for path in segment_filenames]
+        segment_filenames = [path.replace('\\', '/') for path in segment_filenames]
 
-        result["segments"].sort(key=lambda x: x['duration'], reverse=True)
+        np.random.shuffle(segment_filenames)
 
         split_id = int(0.95 * len(result["segments"]))
         train_segments = result["segments"][:split_id]
